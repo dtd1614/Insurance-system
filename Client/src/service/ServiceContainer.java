b511467379dbfcdd7e9f5b6ac89bcfbe8a6e23a0
June <dtd1614@gmail.com>
2023-06-08 04:07:28 +0900

    리팩토링

--- Client/src/service/ServiceContainer.java
+++ Client/src/service/ServiceContainer.java
@@ -24,23 +24,41 @@ public class ServiceContainer {
 
     public void connect(){
         try {this.accidentService = (AccidentServiceIF) Naming.lookup("accidentService");}
-        catch (NotBoundException | MalformedURLException | RemoteException e) {}
+        catch (NotBoundException | MalformedURLException | RemoteException e) {
+            System.out.println("! 사고 서버가 연결되지 않았습니다. 서비스 이용이 일부 제한될 수 있습니다.");
+        }
         try {this.compensateService = (CompensateServiceIF) Naming.lookup("compensateService");}
-        catch (NotBoundException | MalformedURLException | RemoteException e) {}
+        catch (NotBoundException | MalformedURLException | RemoteException e) {
+            System.out.println("! 보상 서버가 연결되지 않았습니다. 서비스 이용이 일부 제한될 수 있습니다.");
+        }
         try {this.customerService = (CustomerServiceIF) Naming.lookup("customerService");}
-        catch (NotBoundException | MalformedURLException | RemoteException e) {}
+        catch (NotBoundException | MalformedURLException | RemoteException e) {
+            System.out.println("! 고객 서버가 연결되지 않았습니다. 서비스 이용이 일부 제한될 수 있습니다.");
+        }
         try {this.contractService = (ContractServiceIF) Naming.lookup("contractService");}
-        catch (NotBoundException | MalformedURLException | RemoteException e) {}
+        catch (NotBoundException | MalformedURLException | RemoteException e) {
+            System.out.println("! 계약 서버가 연결되지 않았습니다. 서비스 이용이 일부 제한될 수 있습니다.");
+        }
         try {this.payService = (PayServiceIF) Naming.lookup("payService");}
-        catch (NotBoundException | MalformedURLException | RemoteException e) {}
+        catch (NotBoundException | MalformedURLException | RemoteException e) {
+            System.out.println("! 납부 서버가 연결되지 않았습니다. 서비스 이용이 일부 제한될 수 있습니다.");
+        }
         try {this.calculationFormulaService = (CalculationFormulaServiceIF) Naming.lookup("calculationFormulaService");}
-        catch (NotBoundException | MalformedURLException | RemoteException e) {}
+        catch (NotBoundException | MalformedURLException | RemoteException e) {
+            System.out.println("! 계산식 서버가 연결되지 않았습니다. 서비스 이용이 일부 제한될 수 있습니다.");
+        }
         try {this.insuranceService = (InsuranceServiceIF) Naming.lookup("insuranceService");}
-        catch (NotBoundException | MalformedURLException | RemoteException e) {}
+        catch (NotBoundException | MalformedURLException | RemoteException e) {
+            System.out.println("! 보험 서버가 연결되지 않았습니다. 서비스 이용이 일부 제한될 수 있습니다.");
+        }
         try {this.employeeService = (EmployeeServiceIF) Naming.lookup("employeeService");}
-        catch (NotBoundException | MalformedURLException | RemoteException e) {}
+        catch (NotBoundException | MalformedURLException | RemoteException e) {
+            System.out.println("! 직원 서버가 연결되지 않았습니다. 서비스 이용이 일부 제한될 수 있습니다.");
+        }
         try {this.saleService = (SaleServiceIF) Naming.lookup("saleService");}
-        catch (NotBoundException | MalformedURLException | RemoteException e) {}
+        catch (NotBoundException | MalformedURLException | RemoteException e) {
+            System.out.println("! 영업 서버가 연결되지 않았습니다. 서비스 이용이 일부 제한될 수 있습니다.");
+        }
 
         try {this.compensateService.setAccidentService(this.accidentService);}
         catch (RemoteException | NullPointerException e) {}
